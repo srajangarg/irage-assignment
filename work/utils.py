5c356943b42b7381c6e2e82c73bb1541b0e07d76
Ayush Gupta <ayushabg@gmail.com>
2021-09-03 17:56:09 -0700

    move greeks stuff to utils

--- work/utils.py
+++ work/utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import py_vollib_vectorized
 from datetime import datetime, timedelta
+import scipy
 
 def create_market_state(BCAST_FILE, expiry_time, get_greeks=True, mf_lower = -0.2, mf_upper = 0.2, RESAMPLING = '30s'):
     '''
@@ -38,3 +39,82 @@ def create_market_state(BCAST_FILE, expiry_time, get_greeks=True, mf_lower = -0.
     curve_state['spread_iv'] = curve_state.ask_iv - curve_state.bid_iv
 
     return curve_state
+
+
+def get_all_greeks_and_black_scholes(df):
+    # s/k vector
+    s_vec = df.futPrice.values
+    k_vec = df.strike.values
+    d_sk = np.log(np.divide(s_vec,k_vec))
+    nlen = df.futPrice.values.shape[0]
+    # interest rate
+    r = np.zeros(nlen)
+    # dividends
+    q = np.zeros(nlen)
+    sigma = df.fit_iv.values
+    t1 = df.time_to_expiry.values
+    # Calculate d1
+    d1 = (d_sk+((r-q+np.power(sigma,2)/2)*(t1)))/(sigma*np.power(t1,0.5))
+    # Calculate cdf of d1
+    cdf_d1 = scipy.stats.norm.cdf(d1)
+    df['cdf_d1'] = d1
+    # Delta in Put is CDF(-d1)
+    df['cdf_d1_signed']  = np.where(df['instrument'] == 'c', cdf_d1, (1-cdf_d1))
+    e_qt = np.exp(-1*q*t1)
+    e_rt = np.exp(-1*r*t1)
+    df['e_qt'] = e_qt
+    # Delta in Put has negative sign
+    df['e_qt_with_delta_sign']     = np.where(df['instrument'] == 'c', e_qt, -1*e_qt)
+    df['delta'] = df['e_qt_with_delta_sign']*df['cdf_d1_signed'] 
+
+
+    # Calculate d2 using d1
+    d2 = d1-sigma*np.power(t1,0.5)
+    cdf_d2 = scipy.stats.norm.cdf(d2)
+    df['cdf_d2'] =  cdf_d2
+    # use vega from wikipedia
+    vega1 = s_vec*e_qt*cdf_d1*np.power(t1,0.5)
+    vega2 = k_vec*e_rt*cdf_d2*np.power(t1,0.5)
+    df['vega1'] = vega1
+    df['vega2'] = vega2
+
+
+    # rho has negative d2 in put
+    df['cdf_d2_signed'] =  np.where(df['instrument'] == 'c', cdf_d2, (1-cdf_d2))
+    rho_unsigned = df.cdf_d2_signed.values * t1*k_vec*e_rt
+    df['rho_unsigned'] = rho_unsigned
+    # rho has a negative sign outside in put formula
+    df['rho'] =  np.where(df['instrument'] == 'c', rho_unsigned, -1*rho_unsigned)
+
+
+    # gamma - formula 1
+    df['gamma1'] = np.divide(e_qt*cdf_d1,s_vec*sigma*np.power(t1,0.5))
+    df['gamma2'] = np.divide(k_vec*e_rt*cdf_d2,sigma*np.power(t1,0.5)*np.power(s_vec,2))
+
+    theta_t1 = -1 * np.divide((e_qt*s_vec*cdf_d1*sigma),(2*np.power(t1,0.5)))
+
+    cdf_d2_signed = df['cdf_d2_signed'].values
+    cdf_d1_signed = df['cdf_d1_signed'].values
+    theta_t2 = -1*r*k_vec*e_rt*cdf_d2_signed
+    theta_t3 = q*s_vec*e_qt*cdf_d1_signed
+    theta_t23 = theta_t2+theta_t3
+
+    df['theta_t1'] = theta_t1
+    df['theta_t23'] = theta_t23
+    df['theta_t23_signed'] =  np.where(df['instrument'] == 'c', theta_t23, -1*theta_t23)
+
+    df['theta'] = df['theta_t23_signed'].values+theta_t1
+
+    price_unsigned = s_vec*np.exp((r-q)*t1)*cdf_d1_signed-k_vec*cdf_d2_signed
+    df['price_unsigned'] = price_unsigned
+    df['price_undiscounted'] =  np.where(df['instrument'] == 'c', price_unsigned, -1*price_unsigned)
+
+    df['price'] =  df.price_undiscounted.values*e_rt
+
+    df2 = df[['delta',  'gamma1','gamma2',  'theta',    'rho',  'vega1','vega2']]
+    df3 = df[['price']]
+    # df3
+
+    return df2, df3
+
+
